9b9d3274797e6eac9a7c4a8a39c1a1bea3271f7f
Frederick Katsura <katsurinstudios@protonmail.ch>
2021-12-02 09:24:32 -0400

    Refactored Part2 Solution

--- src/Day 1/part2.ts
+++ src/Day 1/part2.ts
@@ -3,22 +3,17 @@ import { readFileSync } from "fs";
 const values = readFileSync("./inputs/Day1.txt", "utf8").split("\n");
 
 let counter = 0;
-let newCombo = [];
-let oldCombo = [Number(values[0]), Number(values[1]), Number(values[2])];
+let oldCombo = Number(values[0]);
 
 for (let i = 1; i < values.length; i++) {
-	newCombo = [Number(values[i]), Number(values[i + 1]), Number(values[i + 2])];
+	const newCombo = Number(values[i + 2]);
 
-	if (Number.isNaN(newCombo[0]) || Number.isNaN(newCombo[1]) || Number.isNaN(newCombo[2])) {
+	if (Number.isNaN(newCombo)) {
 		continue;
 	}
-
-	if (
-		newCombo.reduce((partial, newNum) => partial + newNum, 0) >
-		oldCombo.reduce((partial, newNum) => partial + newNum, 0)
-	) {
+	if (oldCombo < newCombo) {
 		counter++;
 	}
-	oldCombo = newCombo;
+	oldCombo = Number(values[i]);
 }
 console.log(counter);
