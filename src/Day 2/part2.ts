3499da10bc1860805b2d200bb2e1b63893ccbb94
Frederick Katsura <katsurinstudios@protonmail.ch>
2021-12-02 16:41:45 -0400

    Refactored Day 2

--- src/Day 2/part2.ts
+++ src/Day 2/part2.ts
@@ -1,26 +1,25 @@
 import { readFileSync } from "fs";
 
-const values = readFileSync("./inputs/Day2.txt", "utf8")
-	.split("\n")
-	.map((value) => value.split(" "));
-
 let forward = 0;
 let depth = 0;
 let aim = 0;
-values.forEach((direction) => {
-	const currNum = Number(direction[1]);
-	switch (direction[0]) {
-		case "forward":
-			forward += currNum;
-			depth += currNum * aim;
-			break;
-		case "up":
-			aim -= currNum;
-			break;
-		case "down":
-			aim += currNum;
-			break;
-	}
-});
+readFileSync("./inputs/Day2.txt", "utf8")
+	.split("\n")
+	.map((value) => value.split(" "))
+	.forEach((direction) => {
+		const currNum = Number(direction[1]);
+		switch (direction[0]) {
+			case "forward":
+				forward += currNum;
+				depth += currNum * aim;
+				break;
+			case "up":
+				aim -= currNum;
+				break;
+			case "down":
+				aim += currNum;
+				break;
+		}
+	});
 
 console.log(forward * depth);
